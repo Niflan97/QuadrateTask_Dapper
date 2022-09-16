10a2e21169ed9e1436b43c710fa418d07aa43e4e
niflan-mhd <niflannif2@gmail.com>
2022-09-16 22:21:18 +0530

    Initial commit

---
+++ Movie.cs
@@ -0,0 +1,11 @@
+namespace DapperWithCRUD;
+
+public class Movies
+{
+    public int Id { get; set; }
+    public string Movie_Name { get; set; } = string.Empty;
+    public string Director { get; set; } = string.Empty;
+    public int Year { get; set; } 
+
+
+}
